13b36fe9725bdc5930d4ccb59e6100e22423a661
visualstudio <visualstudio@122-PC28>
2019-04-10 10:30:38 -0400

    SECTION 2

--- C0725866-Assignment4/Program.cs
+++ C0725866-Assignment4/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleApp1
     class Program
     {
         ArrayList Beowulf;
+        int counterletters = 0;
+        int countSpaces = 0;
         static void Main(string[] args)
         {
             Program a = new Program();
@@ -27,18 +29,44 @@ namespace ConsoleApp1
             {
                 string line;
                 int counter = 0;
+                int a = 0, myWord = 1;
 
                 while ((line = sr.ReadLine()) != null)
                 {
                     Console.WriteLine(line);
                     Beowulf.Add(line);
+                    FindNumberOfBlankSpaces(line);
                     counter++;
+
+                    while (a <= line.Length - 1)
+                    {
+                        if (line[a] == ' ' || line[a] == '\n' || line[a] == '\t')
+                        {
+                            myWord++;
+                        }
+                        a++;
+                    }
+                    a = 0;
+
                 }
+                
 
                 Console.WriteLine("\n\n\n\n********************************The number of lines in the paragraph is " + counter);
+                Console.WriteLine("The number of words in paragraph is " + myWord);
                 Console.ReadLine();
             }
 
+        }
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            
+
+            foreach(char c in line)
+            {
+                if (char.IsLetter(c)) { counterletters++; }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+            }
+            return countSpaces;
         }
     }
 }
