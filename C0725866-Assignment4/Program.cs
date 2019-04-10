57d24d809ccce81189cf32fb05233beec9b64252
visualstudio <visualstudio@122-PC28>
2019-04-10 10:04:37 -0400

    Starter code

--- C0725866-Assignment4/Program.cs
+++ C0725866-Assignment4/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace C0725866_Assignment4
+
+namespace ConsoleApp1
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
+            Program a = new Program();
+            a.Beowulf = new ArrayList();
+            a.ReadTextFiles();
+        }
+
+        public void Run() { this.ReadTextFiles(); }
+        public void ReadTextFiles()
+        {
+
+            using (StreamReader sr = new StreamReader("U:/Users/725866/beowulf.txt"))
+            {
+                string line;
+                int counter = 0;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    Beowulf.Add(line);
+                }
+                
+            }
+
         }
     }
 }
